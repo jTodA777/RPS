cfaa43235b3623998c7dff40d5bd9a7a88872e71
Gyuyeop Kim <rlarbduq777@gmail.com>
2021-05-12 18:13:40 +0900

    [GYKIM] nthread update

--- DrawPicRPS.m
+++ DrawPicRPS.m
@@ -1,13 +1,44 @@
-for i = 1 :1: inf
-    try
-        A = readmatrix(['data/' num2str(i) '.csv']);
-    catch
-        break
+color = {'k','r','g','b'}
+colormap_rgb = [0 0 0;1 0 0; 0 1 0; 0 0 1];
+colormap(colormap_rgb)
+FolderName = 'data64_1.5_1/'
+stepsize = 30
+
+while true
+    clf
+    HowManyFiles = sum(cellfun(@(x) isfile(x),cellfun(@(x) [FolderName x],{dir(FolderName).name},'UniformOutput',false)));
+    population = zeros(HowManyFiles,4);
+    for i = stepsize :stepsize: HowManyFiles
+        try
+            A = readmatrix([FolderName num2str(i) '.csv']);
+            
+            subplot(2,2,4)
+            t = arrayfun(@(x) sum(A==x,'all'),[0 1 2 3]);
+            population(i,:) = t;
+            for j = 1:4
+                plot(stepsize :stepsize:i,population(stepsize:stepsize:i,j),color{j})
+                hold on
+            end
+            
+            xlim([i-50*stepsize i])
+            hold off
+            subplot(2,2,2)
+            for j = 1:4
+                plot(stepsize :stepsize:i,population(stepsize:stepsize:i,j),color{j})
+                hold on
+            end
+            
+            xlim([0 i])
+            hold off
+        catch
+            break
+        end
+        
+        subplot(2,2,[1,3])
+        imagesc(A,[0 3])
+        title(num2str(i))
+        %     pause(0.05)
+        drawnow
+        
     end
-    imagesc(A)
-    colorbar
-    title(num2str(i))
-%     pause(0.05)
-    drawnow
-    
 end
--- RpsGame_Basic.jl
+++ RpsGame_Basic.jl
@@ -1,9 +1,10 @@
 # RPS GAME 
 using DelimitedFiles, Random, StatsBase
-using CSV, DataFrames
 # μ : 먹음, Predation
 # σ : 생성, Reproduction
 # ϵ : 자리바꿈, Exchange
+using Profile
+
 
 function InitMaker(L, PopulationRatio = [0.3, 0.3, 0.3])
     CumPopulationRatio = cumsum(PopulationRatio)
@@ -12,9 +13,9 @@ function InitMaker(L, PopulationRatio = [0.3, 0.3, 0.3])
         error("인구 비율이 맞지 않습니다.")
     end
     
-    State = zeros(L, L)
+    State = zeros(Int64, L, L)
     temp_idx = 1
-    for i = round.(Int,ceil.(length(State) * CumPopulationRatio)[end:-1:1])
+    for i = round.(Int, ceil.(length(State) * CumPopulationRatio)[end:-1:1])
         State[1:i] .= temp_idx
         temp_idx += 1
     end
@@ -22,22 +23,17 @@ function InitMaker(L, PopulationRatio = [0.3, 0.3, 0.3])
     return State
 end
 
-function Predation(State1, State2,NumSpe)
-    if State1 != State2 && State1 != 0 && State2 != 0
-        if (State1 == State2 + 1) || (State1 == 1 && State2 == NumSpe)
-            State1 = 0
-        elseif State2 == State1 + 1
-            State2 = 0
-        end
+function Predation(State1, State2, NumSpe)
+    # 큰 수가 작은 수를 잡아먹음. 
+    if State1 !== 0 && State2 !== 0 && ((State1 === State2 + 1) || (State1 === 1 && State2 === NumSpe))
+        State2 = 0
     end
     return State1, State2
 end
 
 
 function Reproduction(State1, State2)
-    if State1 == 0 
-        State1 = State2
-    elseif State2 == 0
+    if State2 == 0
         State2 = State1
     end
 
@@ -50,26 +46,26 @@ end
 
 function FindMatchingLocation(LocX, LocY, Compass, L)
     LocX̃, LocỸ = LocX, LocY
-    if Compass == "E" 
-        if LocX == L
+    if Compass === "E" 
+        if LocX === L
             LocX̃ = 1
         else
             LocX̃ = LocX + 1
         end
-    elseif Compass == "W"
-        if LocX == 1
+    elseif Compass === "W"
+        if LocX === 1
             LocX̃ = L
         else
             LocX̃ = LocX - 1
         end
-    elseif Compass == "N"
-        if LocY == L
+    elseif Compass === "N"
+        if LocY === L
             LocỸ = 1
         else
             LocỸ = LocY + 1
         end
     else # south
-        if LocY == 1
+        if LocY === 1
             LocỸ = L
         else
             LocỸ = LocY - 1
@@ -78,10 +74,10 @@ function FindMatchingLocation(LocX, LocY, Compass, L)
     return LocX̃, LocỸ
 end
 
-function PairwiseInteraction(LocX, LocY, Compass, ActionProb, L, State, Nμ, Nσ, Nϵ,NumSpe)
+function PairwiseInteraction(LocX, LocY, Compass, ActionProb, L, State, Nμ, Nσ, Nϵ, NumSpe)
     LocX̃, LocỸ = FindMatchingLocation(LocX, LocY, Compass, L)
     if ActionProb < Nμ
-        State[LocX,LocY], State[LocX̃, LocỸ] = Predation(State[LocX,LocY], State[LocX̃, LocỸ],NumSpe)
+        State[LocX,LocY], State[LocX̃, LocỸ] = Predation(State[LocX,LocY], State[LocX̃, LocỸ], NumSpe)
     elseif ActionProb < Nμ + Nσ
         State[LocX,LocY], State[LocX̃, LocỸ] = Reproduction(State[LocX,LocY], State[LocX̃, LocỸ])
     else
@@ -90,30 +86,38 @@ function PairwiseInteraction(LocX, LocY, Compass, ActionProb, L, State, Nμ, Nσ
     return State
 end
 
-function Generation(L, State, Nμ, Nσ, Nϵ,NumSpe,WeightsList,SpeciesList,IdxMatrix)
+function Generation(L, State, Nμ, Nσ, Nϵ, NumSpe, WeightsList, SpeciesList, IdxMatrix)
     Compass = rand(["N" "S" "W" "E"], L^2, 1)
     ActionProb = rand(L^2, 1)
-    WeightsSpecies = zeros(L,L)
+    WeightsSpecies = zeros(L, L)
+    # time1 = 0
+    # time2 = 0
+    # time3 = 0
+    # time = @elapsed 
     for i = 1:L^2
         # Loc = rand( IdxMatrix[State .== sample(SpeciesList, Weights(WeightsList))]) # 개체 수 무시 방법
         # println(State[IdxMatrix[1,1]])
-        if i == mod(i,L)
-            print("|")
-        end
-        for j = SpeciesList
-            for k = IdxMatrix[State .== j]
-                
-                WeightsSpecies[k[1],k[2]] = WeightsList[j]
-            end
-        end
-        Loc = sample(IdxMatrix[:],Weights(WeightsSpecies[:])) # 인구 수 고려
         
-        State = PairwiseInteraction(Loc[1], Loc[2], Compass[i], ActionProb[i], L, State, Nμ, Nσ, Nϵ,NumSpe) 
+        for j = SpeciesList
+            WeightsSpecies[State .=== j] .= WeightsList[j]
+            # for k = IdxMatrix[State .== j]
+            #     @inbounds WeightsSpecies[k[1],k[2]] = WeightsList[j]
+            # end
+        end
+        W = ProbabilityWeights(WeightsSpecies[:])
+        Loc = sample(IdxMatrix[:], W) # 인구 수 고려
+        
+        State = PairwiseInteraction(Loc[1], Loc[2], Compass[i], ActionProb[i], L, State, Nμ, Nσ, Nϵ, NumSpe) 
+        # if 0 == mod(i, L)
+        #     println(time1,"  ", time2,"  ", time3)
+        # end
+        
     end
+    # print(time, " seconds  ")
     return State
 end
 
-function MainRPS(μ, σ, ϵ, L, TotalIteration,PopulationRatio,WeightsList)
+function MainRPS(FolderName, μ, σ, ϵ, L, PreIteration, TotalIteration, PopulationRatio, WeightsList)
  # Normalized parameters
     Nμ = μ / (μ + σ + ϵ)
     Nσ = σ / (μ + σ + ϵ)
@@ -121,38 +125,46 @@ function MainRPS(μ, σ, ϵ, L, TotalIteration,PopulationRatio,WeightsList)
     
     State = InitMaker(L, PopulationRatio)
     NumSpe = length(PopulationRatio)
-    IdxMatrix = [(x,y) for x=1:L,y=1:L]
-    #count(State.==3)
+    IdxMatrix = [(x, y) for x = 1:L,y = 1:L]
+    # count(State.==3)
     
     SpeciesList = 1:NumSpe
-    
-    if ~isdir("data")
-        mkdir("data")
+    if ~isdir(FolderName)
+        mkdir(FolderName)
     else
-        rm("data", recursive=true)
-        mkdir("data")
+        rm(FolderName, recursive = true)
+        mkdir(FolderName)
     end
 
     for i = 1:TotalIteration
         
-        print(i)
-        print(" ")
-        State = Generation(L, State, Nμ, Nσ, Nϵ,NumSpe,WeightsList,SpeciesList,IdxMatrix)
-        FileName = "data\\" * string(i) * ".csv"
+        # print(WeightsList[1]," // ", i,"    //    ", "계산 중... ")
         
-        print(typeof(State))
-        break
-        print(" ")
-        open(FileName, "w") do io
-            CSV.write(io, DataFrame(State, :auto))
-        end
+        
+        FileName = "\\" * string(i) * ".csv"
+        
+        
+        State = Generation(L, State, Nμ, Nσ, Nϵ, NumSpe, WeightsList, SpeciesList, IdxMatrix)
+        
+        # print("계산 완료  // ")
+        # println(State)
+        
         # open(FileName, "w") do io
-        #     writedlm(io, State)
+        #     CSV.write(io, DataFrame(State, :auto))
         # end
-        println()
+        
+        if i > PreIteration
+            # print("작성 중... ")
+            open(FolderName * FileName, "w") do io
+                writedlm(io, State)
+            end
+            
+            # println(" 작성 완료")
+        end
+        println(WeightsList[1], " // ", i)
     end
 end
 
-
-MainRPS(1, 1, 3 * 10^-6, 512, 5000,[0.3,0.3,0.3],[0.1,1,1])
-
+Threads.@threads for i = 1:0.01:3
+    MainRPS("data64_" * string(i), 1, 1, 3 * 10^-6, 64, 0, 10000, [0.3,0.3,0.3], [i,1,1])
+end
